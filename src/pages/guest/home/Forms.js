f36bc99b580485dae0010f36a4e8143547540677
aryumne <aryumsf@gmail.com>
2022-08-11 11:18:30 +0900

    update logic submit form

--- src/pages/guest/home/Forms.js
+++ src/pages/guest/home/Forms.js
@@ -64,7 +64,7 @@ const FormSikoja = () => {
     });
 
     const thumbs = files.map(file => {
-        if (file.type == 'video/mp4') {
+        if (file.type === 'video/mp4') {
             return (
                 <ReactPlayer key={file.name} height='100%' width='100%' controls url={file.preview} />
             )
@@ -110,36 +110,39 @@ const FormSikoja = () => {
 
     const handleOnSubmit = (event) => {
         event.preventDefault();
-        files.length === 0 ? setMessage({ code: 400, msg: 'Upload gambar/video sebagai bukti pengaduan', status: true }) :
+        if (files.length === 0) {
+            setMessage({ code: 400, msg: 'Upload gambar/video sebagai bukti pengaduan', status: true })
+        } else {
             setOpen(true)
-        APISTORE.StoreSibolang(data).then(result => {
-            // console.log(result.data);
-            setMessage({ code: 201, msg: "Laporan telah disampaikan", status: true });
-            for (let file of files) {
-                const data2 = new FormData();
-                data2.append('galery', file)
-                data2.append('sibolang_id', result.data.id)
-                APIUPLOAD.UploadGalery(data2).then(() => {
-                    setData({
-                        title: '',
-                        description: '',
-                        village_id: null,
-                        category_id: null,
-                        name: '',
-                        hp: null,
-                    });
-                    setFiles([]);
-                }).catch(() => {
-                    setOpen(false)
-                    setMessage({ code: 400, msg: 'Gagal mengupload, coba lagi!', status: true })
-                })
-            }
-            setOpen(false)
-        }).catch((error) => {
-            console.log(error)
-            setMessage({ code: 400, msg: 'Gagal mengupload, coba lagi!', status: true })
-            setOpen(false)
-        });
+            APISTORE.StoreSibolang(data).then(result => {
+                // console.log(result.data);
+                setMessage({ code: 201, msg: "Laporan telah disampaikan", status: true });
+                for (let file of files) {
+                    const data2 = new FormData();
+                    data2.append('galery', file)
+                    data2.append('sibolang_id', result.data.id)
+                    APIUPLOAD.UploadGalery(data2).then(() => {
+                        setData({
+                            title: '',
+                            description: '',
+                            village_id: null,
+                            category_id: null,
+                            name: '',
+                            hp: null,
+                        });
+                        setFiles([]);
+                    }).catch(() => {
+                        setOpen(false)
+                        setMessage({ code: 400, msg: 'Gagal mengupload, coba lagi!', status: true })
+                    })
+                }
+                setOpen(false)
+            }).catch((error) => {
+                console.log(error)
+                setMessage({ code: 400, msg: 'Gagal mengupload, coba lagi!', status: true })
+                setOpen(false)
+            });
+        }
     }
 
     return (
@@ -158,7 +161,7 @@ const FormSikoja = () => {
                         <form onSubmit={handleOnSubmit}>
                             <CardContent>
                                 <Typograph text="SIBOLANG" gutterBottom variant="h5" textTransform='uppercase' fontWeight='bold' sx={{ py: 2 }} />
-                                <Alert severity={message.code == 201 ? 'success' : 'error'} sx={{ mb: 2, display: `${message.status ? 'flex' : 'none'}` }} >{message.msg}</Alert>
+                                <Alert severity={message.code === 201 ? 'success' : 'error'} sx={{ mb: 2, display: `${message.status ? 'flex' : 'none'}` }} >{message.msg}</Alert>
                                 <FormControl fullWidth >
                                     <FormLabel id="radion-button" sx={{ mb: 1 }}>Pilih kategori Pengaduan!</FormLabel>
                                     <RadioGroup
